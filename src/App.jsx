b649768cf8c98dbbb39de93e0a256bc4451e695f
dhanJi_exp <kdhanji183@gmail.com>
2025-08-17 16:54:17 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
 
 export default function App() {
   return (
-    <div className='bg-blue-50 text-2xl'>Hello world</div>
+    <div className='bg-blue-50 text-2xl'>
+      <div>
+        <h1 className="text-2xl text-green-700"> Avinash push </h1>
+      </div>
+    </div>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+export const Container = ({ children }) => {
+    return (
+        <div className="max-w-7xl px-4 h-full mx-auto">
+            {children}
+        </div>
+    )
+}
